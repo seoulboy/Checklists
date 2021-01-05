86a37517cf16665c7fa2834eabde7d18246f0187
Eric Jang <940709@gmail.com>
2021-01-05 15:30:54 +0900

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Eric Jang on 2021/01/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
