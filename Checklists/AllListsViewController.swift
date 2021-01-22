27e3183d922f808aff96b35b922b60e7ba03854a
Eric Jang <940709@gmail.com>
2021-01-22 23:57:51 +0900

    completed Section 17 Improved Data Model - Part 1

--- Checklists/AllListsViewController.swift
+++ Checklists/AllListsViewController.swift
@@ -8,28 +8,33 @@
 import UIKit
 
 class AllListsViewController: UITableViewController, ListDetailViewControllerDelegate {
-    
     let cellIdentifier = "ChecklistCell"
     var lists = [Checklist]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.register(UITableViewCell.self,forCellReuseIdentifier: cellIdentifier)
         navigationController?.navigationBar.prefersLargeTitles = true
+        tableView.register(UITableViewCell.self,forCellReuseIdentifier: cellIdentifier)
         
-        var list = Checklist(name: "Birthdays")
-        lists.append(list)
-        
-        list = Checklist(name: "Groceries")
-        lists.append(list)
-        
-        list = Checklist(name: "Cool Apps")
-        lists.append(list)
-        
-        list = Checklist(name: "To Do")
-        lists.append(list)
+        // Load data
+        loadChecklists()
     }
     
+    // MARK: - Navigation
+    override func prepare(
+        for segue: UIStoryboardSegue,
+        sender: Any?
+    ) {
+        if segue.identifier == "ShowChecklist" {
+            let controller = segue.destination as! ChecklistViewController
+            controller.checklist = sender as? Checklist
+        } else if segue.identifier == "AddChecklist" {
+            let controller = segue.destination as! ListDetailViewController
+            controller.delegate = self
+        }
+    }
+    
+    // MARK: - Table view data source
     override func tableView(
         _ tableView: UITableView,
         numberOfRowsInSection section: Int
@@ -59,21 +64,32 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         let checklist = lists[indexPath.row]
         performSegue(withIdentifier: "ShowChecklist", sender: checklist)
     }
-    
-    // MARK: - Navigation
-    override func prepare(
-        for segue: UIStoryboardSegue,
-        sender: Any?
-    ) {
-        if segue.identifier == "ShowChecklist" {
-            let controller = segue.destination as! ChecklistViewController
-            controller.checklist = sender as? Checklist
-        } else if segue.identifier == "AddChecklist" {
-            let controller = segue.destination as! ListDetailViewController
-            controller.delegate = self
-        }
-    }
 
+    
+    override func tableView(
+        _ tableView: UITableView,
+        commit editingStyle: UITableViewCell.EditingStyle,
+        forRowAt indexPath: IndexPath
+    ) {
+        lists.remove(at: indexPath.row)
+        
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+    }
+    
+    override func tableView(
+        _ tableView: UITableView,
+        accessoryButtonTappedForRowWith indexPath: IndexPath
+    ) {
+        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
+        controller.delegate = self
+        
+        let checklist = lists[indexPath.row]
+        controller.checklistToEdit = checklist
+        
+        navigationController?.pushViewController(controller, animated: true)
+    }
+    
     // MARK: - List Detail View Controller Delegates
     func listDetailViewControllerDidCancel(
         _ controller: ListDetailViewController
@@ -108,27 +124,40 @@ class AllListsViewController: UITableViewController, ListDetailViewControllerDel
         navigationController?.popViewController(animated: true)
     }
     
-    override func tableView(
-        _ tableView: UITableView,
-        commit editingStyle: UITableViewCell.EditingStyle,
-        forRowAt indexPath: IndexPath
-    ) {
-        lists.remove(at: indexPath.row)
-        
-        let indexPaths = [indexPath]
-        tableView.deleteRows(at: indexPaths, with: .automatic)
+    // MARK: - Data Saving
+    func documentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
     
-    override func tableView(
-        _ tableView: UITableView,
-        accessoryButtonTappedForRowWith indexPath: IndexPath
-    ) {
-        let controller = storyboard!.instantiateViewController(withIdentifier: "ListDetailViewController") as! ListDetailViewController
-        controller.delegate = self
-        
-        let checklist = lists[indexPath.row]
-        controller.checklistToEdit = checklist
-        
-        navigationController?.pushViewController(controller, animated: true)
+    func dataFilePath() -> URL {
+        return documentsDirectory().appendingPathComponent("Checklists.plist")
     }
+    
+    // this method is now called saveChecklists()
+    func saveChecklists() {
+        let encoder = PropertyListEncoder()
+        do {
+            // you encode lists instead of "items"
+            let data = try encoder.encode(lists)
+            try data.write(to: dataFilePath(), options: Data.WritingOptions.atomic)
+        } catch {
+            print("Error encoding list array: \(error.localizedDescription)")
+        }
+    }
+    
+    // this method is now called loadChecklists()
+    func loadChecklists() {
+        let path = dataFilePath()
+        if let data = try? Data(contentsOf: path) {
+            let decoder = PropertyListDecoder()
+            do {
+                lists = try decoder.decode([Checklist].self, from: data)
+            } catch {
+                print("Error decoding list array: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    
 }
